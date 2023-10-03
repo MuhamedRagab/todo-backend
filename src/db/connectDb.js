fdea0be88b72ce173320c7fde817fd4e4318cfd2
MuhamedRagab <mr2837465@gmail.com>
2023-10-03 07:22:40 +0300

    add mongoose options

--- src/db/connectDb.js
+++ src/db/connectDb.js
@@ -2,7 +2,13 @@ const { default: mongoose } = require("mongoose");
 
 const MONGO_URI = process.env.MONGO_URI;
 const connectDb = () => {
-  mongoose.connect(MONGO_URI);
+  mongoose.connect(MONGO_URI),
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    };
 
   const db = mongoose.connection;
 
